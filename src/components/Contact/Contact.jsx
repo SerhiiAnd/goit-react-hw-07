9ed99650bf8664d3d435ec3e40c5ec5d8c12dbec
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2024-04-06 15:52:57 +0200

    06.04-save2

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,33 @@
+import css from "./Contact.module.css";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
+
+export default function Contact({ data }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className={css.contact}>
+      <div>
+        <div className={css.container}>
+          <span role="img" aria-label="person">
+            👤
+          </span>{" "}
+          {data.name}
+          <h1 className={css.text}></h1>
+        </div>
+
+        <div className={css.container}>
+          ☎️
+          <p className={css.text}>{data.number}</p>
+        </div>
+      </div>
+
+      <button
+        onClick={() => dispatch(deleteContact(data.id))}
+        className={css.button}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
