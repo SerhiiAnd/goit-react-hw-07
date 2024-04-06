840ff71b50ea5caf9c73710e8831032a51a2056f
SerhiiAnd <136363351+SerhiiAnd@users.noreply.github.com>
2024-04-06 22:36:25 +0200

    06.04-save5

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,13 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-
-// const contactsSlice = createSlice({
-//   name: "contacts",
-//   initialState: {
-//     items: [
-//       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-//       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-//       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchContacts, addContact, deleteContact } from "./contactsOps";
 import { selectNameFilter } from "./filtersSlice.js";
